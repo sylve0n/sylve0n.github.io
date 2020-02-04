81d6b7b59674be13c08e3e61f7225de211682de6
Hannah Sartin <hnsartin@gmail.com>
2020-02-04 11:35:08 -0600

    removed math.floor for even more randomness

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,8 @@ var glitchText = document.querySelector('#glitch-text');
 var style = document.createElement('style');
 
 function setRandomAnimationDuration() {
-  var randomNumAfter = Math.floor(Math.random() * 3 + 1) + 's';
-  var randomNumBefore = Math.floor(Math.random() * 3 + 1) + 's';
+  var randomNumAfter = Math.random() * 3 + 1 + 's';
+  var randomNumBefore = Math.random() * 3 + 1 + 's';
   style.innerHTML =
   "#glitch-text::after {animation: glitch-anim " + randomNumAfter + " infinite;} #glitch-text::before {animation: glitch-anim " + randomNumBefore + " infinite;}"
   document.head.appendChild(style);
