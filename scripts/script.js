c980f2a79cb6f5d6e1089a57adf0a4e8ec0008d6
Hannah Sartin <hnsartin@gmail.com>
2020-02-04 11:34:10 -0600

    making it the twin peaks animated thing again. spent way too long making the animation random.

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+var glitchText = document.querySelector('#glitch-text');
+var style = document.createElement('style');
+
+function setRandomAnimationDuration() {
+  var randomNumAfter = Math.floor(Math.random() * 3 + 1) + 's';
+  var randomNumBefore = Math.floor(Math.random() * 3 + 1) + 's';
+  style.innerHTML =
+  "#glitch-text::after {animation: glitch-anim " + randomNumAfter + " infinite;} #glitch-text::before {animation: glitch-anim " + randomNumBefore + " infinite;}"
+  document.head.appendChild(style);
+}
+
+glitchText.addEventListener('animationiteration', setRandomAnimationDuration);
